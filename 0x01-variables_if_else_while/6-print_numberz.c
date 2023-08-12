a6d801dd338d1166d77765dad480284b01f33099
Tess647 <thanni_taslihah@yahoo.com>
2023-08-11 19:23:17 -0700

    Files added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  *
+  * This program prints out the characters 0-9
+  * Return: Always 0(Success)
+  */
+
+int main(void)
+{
+	char ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  *
+  * This program prints all the numbers of base 16 in lower case
+  * Return: Always 0(Success)
+  */
+
+int main(void)
+{
+	char ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+	}
+	ch = 'a';
+	while (ch <= 'f')
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
