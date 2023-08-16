85d5e67bc0519565f7aae90162f3fd1881240314
Tess647 <thanni_taslihah@yahoo.com>
2023-08-16 11:28:16 -0700

    file added

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,24 +9,20 @@
 int main(void)
 {
 	int i;
-	long int fibonacci [50];
+	long int fibonacci[50];
 
-	fibonacci [0] = 1;
-	fibonacci [1] = 2;
-	printf("%ld\n", fibonacci[0]);
-	printf("%ld\n", fibonacci[1]);
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, ", fibonacci[0]);
+	printf("%ld, ", fibonacci[1]);
 
 	for (i = 2; i < 50; i++)
 	{
-		fibonacci [i] = fibonacci [i-1] + fibonacci [i-2];
+		fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
 		if (i == 49)
 			printf("%ld\n", fibonacci[i]);
 		else
-			printf("%ld", fibonacci[i]);
+			printf("%ld, ", fibonacci[i]);
 	}
 	return (0);
 }
-
-					
-
-
