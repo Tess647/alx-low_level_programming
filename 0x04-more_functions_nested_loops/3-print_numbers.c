4fc9e7cdb9b1f649bdfa26057e9919d480ad6b20
Tess647 <thanni_taslihah@yahoo.com>
2023-08-17 10:37:16 -0700

    fiiles added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints integers from 0-9
+  *
+  * Return: nothing
+  */
+
+void print_numbers()
+{
+	char i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
