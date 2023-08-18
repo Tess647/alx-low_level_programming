134e629abd145db4832db428ffe3c73e5b26fef5
Tess647 <thanni_taslihah@yahoo.com>
2023-08-18 09:20:47 -0700

    Files added

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,20 @@
 
 void print_number(int n)
 {
-	int n1 = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		n1 *= -1;
+		n1 = -n;
 		_putchar('-');
 	}
+	else
+	{
+		n1 = n;
+	}
 	if (n1 / 10)
+	{
 		print_number(n1 / 10);
+	}
 	_putchar((n1 % 10) + '0');
 }
