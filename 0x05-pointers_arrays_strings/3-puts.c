58c1f171625363453e2e2bc8a42dd0e2f3b21d66
Tess647 <thanni_taslihah@yahoo.com>
2023-08-21 08:34:14 -0700

    Files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _puts - prints out strings
+  * @str: input parameter(pointer)
+  * Return: Nothing
+  */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str)
+	}
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_rev - prints out strings in reverse order
+  * @s: input parameter(pointer)
+  * Return: Nothing
+  */
+void _puts(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(str)
+	}
+}
