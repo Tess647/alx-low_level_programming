ce3413c6f33be3fe7bc182f42e6dcafb6f586f16
Tess647 <thanni_taslihah@yahoo.com>
2023-08-23 11:19:58 -0700

    files added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
   * _strcmp - function that compares two strings
@@ -16,8 +16,8 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (0);
 		}
-		*s1++;
-		*s2++;
+		s1++;
+		s2++;
 	}
 
 	return (*s1 - *s2);
