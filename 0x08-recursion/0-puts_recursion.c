fd4e92df9ce037fbbf0e8e29376ec7b6bffd10f3
Tess647 <thanni_taslihah@yahoo.com>
2023-08-30 04:17:06 -0700

    files added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - function that prints a string, followed by a new line
+  * @s: pointer to string s to be printed
+  *
+  * Return: Nothing
+  */
+
+void _puts_recursion(char *s)
+{
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+
+	_puts_recursion(*s + 1);
+}
+
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - function that prints a string in reverse
+  * @s: pointer to string s to be printed
+  *
+  * Return: Nothing
+  */
+
+int _strlen_recursion(char *s)
+{
+
+	if (s[i] != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
