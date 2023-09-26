2cfdd201c67e24c8754f3486754ceb31e92ff275
Tess647 <thanni_taslihah@yahoo.com>
2023-09-26 09:23:49 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -21,3 +21,49 @@ void free_listp(listp_t **head)
 		*head = NULL;
 	}
 }
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+	listp_t *Hptr, *new, *add;
+
+	Hptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = Hptr;
+		Hptr = new;
+
+		add = Hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&Hptr);
+				return (nodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
+	}
+
+		free_listp(&Hptr);
+		return (nodes);
+	}
+
