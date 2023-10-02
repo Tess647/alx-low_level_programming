dd4ea84855957036dc2507124aea18b2b77844cf
Tess647 <thanni_taslihah@yahoo.com>
2023-10-02 13:00:54 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,10 +22,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
+		for (num_letters = 0; text_content[num_letters]; num_letters++)
 			;
 
-		rwr = write(fd, text_content, nletters);
+		rwr = write(fd, text_content, num_letters);
 
 		if (rwr == -1)
 			return (-1);
