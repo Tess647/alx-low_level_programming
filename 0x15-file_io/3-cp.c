a93a6fb63208938e7fda8d2b71476b2d837a1061
Tess647 <thanni_taslihah@yahoo.com>
2023-11-05 21:12:21 +0000

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,32 +5,34 @@
  * error_file - checks if files can be opened.
  * @file_from: file_from.
  * @file_to: file_to.
- * @argv: arguments array.
+ * @argv: arguments vector.
  * Return: no return.
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 }
 
 /**
  * main - check the code for Holberton School students.
  * @argc: number of arguments.
- * @argv: arguments array.
+ * @argv: arguments vector.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchar, nwr;
-	char buffer[1024];
+	ssize_t nchars, nwr;
+	char buf[1024];
 
 	if (argc != 3)
 	{
@@ -42,13 +44,13 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchar = 1024;
-	while (nchar == 1024)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		nchar = read(file_from, buffer, 1024);
-		if (nchar == -1)
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buffer, nchar);
+		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
@@ -63,7 +65,7 @@ int main(int argc, char *argv[])
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
